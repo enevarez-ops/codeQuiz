486eba4aae71973f9fffd6713e4e8270833e54ca
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2020-10-12 19:13:45 -0500

    added questions to javascipt

---
+++ script.js
@@ -0,0 +1,32 @@
+let questE1 = [
+    {
+   question: "Inside which HTML element do we put the JavaScript?",
+   choices: ["<script>". "<scripting>", "<java>", "<javascript>"],
+   answer: "<script>"
+    },
+    { 
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: ["<head>", "<body>", "both"],
+    answer: "both"
+    },
+    {
+        question: "The external JavaScript file must contain the <script> tag.",
+        choices: ["False", "True"],
+        answer: "False"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        choices: ["function = exFunction()", "function exFunction()", "function {exFunction}", "function:exFunction()"],
+        answer: "function exfunction()"
+    },
+    {
+        question: "How do you call a function named exFunction?",
+        choices: ["exFunction()", "run exFunction", "call exFunction()",],
+        answer: "exFunction()"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choices: ["//comment", ":comment", "<!--comment-->",],
+        answer: "//comment"
+    }
+ ]
